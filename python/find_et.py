57109b4d6ac26ff504e26fa16df84fb18e320b46
SuzanneEisenberg <137560748+SuzanneEisenberg@users.noreply.github.com>
2024-07-23 15:55:44 -0700

    Update find_et.py

--- python/find_et.py
+++ python/find_et.py
@@ -96,10 +96,10 @@ class find_et(gr.sync_block):
 
 
     def work(self, input_items, output_items):
-        #self.spectra = np.empty((0, self.n_fine_chans), dtype=np.float32, order='C')
+        self.spectra = np.empty((0, self.n_fine_chans), dtype=np.float32, order='C')
         i = 0
         for i in range(61):
-            spectra[i] = input_items[0][i]
+            spectra = input_items[0][i]
             i =+1
 
         if DEBUGGING:
