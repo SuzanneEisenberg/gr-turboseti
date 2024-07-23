668fe5d1d4a33cdb9bab3f2dc7af0b5835863f80
SuzanneEisenberg <137560748+SuzanneEisenberg@users.noreply.github.com>
2024-07-22 19:24:16 -0700

    Update find_et.py

--- python/find_et.py
+++ python/find_et.py
@@ -97,6 +97,7 @@ class find_et(gr.sync_block):
     def work(self, input_items, output_items):
         print ("The edits are showing up!!!!!!!")
         print ("The edits are showing up again!!!!!!")
+        print ("Edits part III")
         if DEBUGGING:
             print("DEBUG findET input_items[0] shape:", input_items[0].shape) #Checks input is expected shape (60, 1e6)
 
